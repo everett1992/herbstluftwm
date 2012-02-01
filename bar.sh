a895b67ffb7b137f17fa44feccc462ddfebf4a63
Caleb Everett <everett.caleb@gmail.com>
2012-02-01 12:01:17 -0500

    updated dependancies

--- bar.sh
+++ bar.sh
@@ -6,7 +6,12 @@
 ## Make sure to run after xrandr_set_monitor.sh    ##
 ###                                               ###
 
-font="-*-dejavu sans mono-medium-r-*-*-*-*-*-*-*-*-*-*"
+font="iceberg"
+dark_bg="#121212"
+light_bg="#2c2c2c"
+light_light="#5f5f5f"
+green="#56e202"
+
 dir=~/.config/herbstluftwm/
 
 function hc() {
@@ -23,12 +28,12 @@ do
   let "width= ${geometry[2]} - 130"
   height=${geometry[3]}
   hc pad $i 16
-  sh $dir/ws_bar $i | dzen2 -bg '#222222' -ta l -w $width -x $x -y $y &
+  sh $dir/ws_bar $i | dzen2 -bg $light_bg  -ta l -w $width -h 16 -x $x -y $y &
   let "x= $x + $width"
-  i3status -c $dir/i3status.conf | dzen2 -bg '#222222' -ta l -x $x -y $y &
+  i3status -c $dir/i3status.conf | dzen2 -bg $light_bg  -ta r -h 16 -x $x -y $y -fn $font &
   if [ "$i" -eq "0" ] ; then
     let "x=$x - 20"
     sleep .1
-    stalonetray -i 16 -bg '#222222' --geometry 1x1+$x+$y --grow-gravity E &
+    stalonetray -i 16 -bg $light_bg --geometry 1x1+$x+$y --grow-gravity E &
   fi
 done
