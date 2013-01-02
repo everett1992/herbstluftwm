026ea1cd88fa7fc79dc6a92f5504cf9855503fa8
Caleb Everett <everett.caleb@gmail.com>
2013-01-01 20:45:07 -0500

    added battery to status bar

--- panel.sh
+++ panel.sh
@@ -19,8 +19,8 @@ let "panel_width= ${geometry[2]}-64"
 panel_height=16
 font="-*-fixed-medium-*-*-*-12-*-*-*-*-*-*-*"
 bgcolor=$COLOR0
-selbg=$COLOR9
-selfg=$COLOR8
+selbg=$COLOR2
+selfg="#000000"
 
 ####
 # Try to find textwidth binary.
@@ -82,7 +82,8 @@ herbstclient pad $monitor $panel_height
         echo -n "$separator"
         echo -n "^bg()^fg() ${windowtitle//^/^^}"
         # small adjustments
-        right="$separator^bg() $date $separator"
+        battery="^fg(#909090)bat: ^fg(#efefef)`acpi -b | grep [0-9]*% -o`"
+        right="$separator^bg() $battery $separator^bg() $date $separator"
         right_text_only=$(echo -n "$right"|sed 's.\^[^(]*([^)]*)..g')
         # get width of right aligned text.. and add some space..
         width=$($textwidth "$font" "$right_text_only    ")
